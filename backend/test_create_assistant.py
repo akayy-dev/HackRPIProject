c9f670d2af4cbecba12882cac0709333519db9ae
ericdeon15 <ericpdeon15@gmail.com>
2024-11-09 16:52:59 -0500

    Altered tests

--- backend/test_create_assistant.py
+++ backend/test_create_assistant.py
@@ -1,4 +1,4 @@
-from gpt import OpenAI
+from openai import OpenAI
 from os import getenv
 
 api_key = getenv("OPENAI_KEY")
@@ -7,7 +7,7 @@ client = OpenAI(api_key=api_key)
 
 assistant = client.beta.assistants.create(
   name="Place Summarizer",
-  instructions="You give summarizes about places in urban areas, placing empahsis on cultural impact and historical notions",
+  instructions="You are a specialized assistant focused on summarizing historical and contextual information about locations. Use only details from the information provided. When provided with detailed information, especially from sources like Wikipedia, highlight key historical events, significant cultural developments, and notable changes over time. Emphasize elements that offer insights into how the location evolved historically and its impact on the present day, while keeping the summary concise and relevant.Structure your response as such: A brief paragraph at the the top detailing the important contextual information of the location. Then have a series of bullet points to follow detailing any interesting facts about the place, starting each bullet point with a relevant emoji. Limit this section to 3-5 bullet points.",
   model="gpt-4o"
 )
 
