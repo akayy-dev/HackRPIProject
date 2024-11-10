1eb2796ab8314a0ef22777311311283429dee804
Ahadu Kebede <ahadukebede@gmail.com>
2024-11-09 23:09:56 -0500

    added chart

--- frontend/src/components/EmissionsTracker.js
+++ frontend/src/components/EmissionsTracker.js
@@ -1,14 +1,17 @@
-// src/components/dashboard/FoodSection.js
-
-import React from 'react';
+import React, { useEffect } from "react";
+import { LineChart } from "react-chartkick";
+import "chartkick/chart.js";
 
 const EmissionsTracker = () => {
-  return (
-    <div className="section">
-      <h2>Emissions Options</h2>
-      <p>Placeholder text for food options.</p>
-    </div>
-  );
+  let data = { "Yesterday": 0,  "Today": 10, "Tomorrow": 5 };
+
+	return (
+		<div className="section">
+			<h2>Emissions Options</h2>
+			<p>Placeholder text for food options.</p>
+      <LineChart data={data} />
+		</div>
+	);
 };
 
 export default EmissionsTracker;
